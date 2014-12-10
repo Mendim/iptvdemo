d5f3ae70baff61c66b634da7314bba3f10241419
playniuniu <playniuniu@gmail.com>
2014-12-10 23:14:47 +0800

    clear other unuseful page

--- app/views/main.py
+++ app/views/main.py
@@ -1,5 +1,5 @@
 # coding: utf-8
-from flask import render_template,  Blueprint, request, abort
+from flask import render_template,  Blueprint
 from flask.ext.login import login_required
 
 mainView = Blueprint('main', __name__)
@@ -14,13 +14,13 @@ def index():
 @login_required
 def tvcrm():
     return render_template('main/pages/tvcrm.html',
-            content_title='收视率分析')
+            content_title='收视率统计')
 
 @mainView.route('/tv_analysis/')
 @login_required
 def tv_analysis():
     return render_template('main/pages/tv_analysis.html',
-            content_title='节目内容分析')
+            content_title='节目内容统计')
 
 @mainView.route('/user_analysis/')
 @login_required
@@ -40,68 +40,14 @@ def time_analysis():
 def user_info(user_id=None):
     if user_id:
         return render_template('main/pages/user_detail.html',
-            content_title='用户深度分析',
+            content_title='深度用户分析',
             content_id=user_id)
 
     return render_template('main/pages/user_info.html',
-        content_title='用户分析')
+        content_title='深度用户分析')
 
-
-@mainView.route('/room/')
-@mainView.route('/room/<int:room_name>')
+@mainView.route('/voidad_analysis/')
 @login_required
-def room(room_name=None):
-    if room_name == 1:
-        return render_template('main/pages/room_detail.html',
-            content_title='石家庄金石机房',
-            web_3d_url = "#")
-
-    return render_template('main/pages/room.html',
-            content_title='机房资源统计')
-
-@mainView.route('/flow/')
-@login_required
-def flow():
-    return render_template('main/pages/flow.html',
-            content_title='流量核算')
-
-@mainView.route('/port/', methods=['GET'])
-@login_required
-def port():
-    try:
-        port_name = request.args.get('port').strip("\"")
-        dev_name = request.args.get('device').strip("\"")
-        month_value = request.args.get('month').strip("\"")
-    except:
-        return abort(404)
-
-    return render_template('main/pages/port.html',
-            port_name = port_name,
-            dev_name = dev_name,
-            month_value = month_value,
-            content_title='端口详情')
-
-@mainView.route('/business/')
-@login_required
-def business():
-    return render_template('main/pages/business.html',
-            content_title='收入评估')
-
-
-@mainView.route('/customer/')
-@login_required
-def customer():
-    return render_template('main/pages/customer.html',
-            content_title='客户评估')
-
-@mainView.route('/analyze-room/')
-@login_required
-def analyze_room():
-    return render_template('main/pages/analyze_room.html',
-            content_title='机房投资分析')
-
-@mainView.route('/analyze-business/')
-@login_required
-def analyze_business():
-    return render_template('main/pages/analyze_business.html',
-            content_title='业务投资分析')
+def voidad_analysis():
+    return render_template('main/pages/voidad_analysis.html',
+            content_title='广告分析')
